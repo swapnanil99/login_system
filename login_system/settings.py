64715c6610026ce1d0d481d95febe6981a23f829
Swapnanil Maity <swapnanilmaity99@gmail.com>
2026-04-27 22:19:09 +0530

    switch to postgres

--- login_system/settings.py
+++ login_system/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 import os
 
 
@@ -74,11 +75,11 @@ WSGI_APPLICATION = 'login_system.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,)
     }
-}
+
 
 
 # Password validation
